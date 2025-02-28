995e38f5e318d3b0869f3cd2adf263875c04777a
paoyako24 <paolaparedes248@gmail.com>
2025-02-28 13:56:58 -0600

    Add files via upload

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss'],
+  styleUrls: ['./cart.component.scss']
 })
 export class CartComponent {
   productos = [
@@ -11,15 +11,24 @@ export class CartComponent {
     { nombre: 'Filtro de Aire', precio: 500, cantidad: 2 }
   ];
 
-  get subtotal() {
+  get subtotal(): number {
     return this.productos.reduce((acc, p) => acc + p.precio * p.cantidad, 0);
   }
 
-  get total() {
+  get total(): number {
     const envio = 250;
     return this.subtotal + envio;
   }
+
+  disminuirCantidad(index: number) {
+    this.productos[index].cantidad = Math.max(1, this.productos[index].cantidad - 1);
+  }
+
+  aumentarCantidad(index: number) {
+    this.productos[index].cantidad += 1;
+  }
 }
+
 
 
 
